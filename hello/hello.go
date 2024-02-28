2fc8993db8cf75019810cfcdac37b5a1c3171828
KrithikJoshi <aimjkrithik@gmail.com>
2024-02-28 23:23:31 +0530

    random greetings and random names generated

--- hello/hello.go
+++ hello/hello.go
@@ -11,12 +11,13 @@ func main() {
     //setting the properties to predefined logger , including the log entry prefix and a flag to disable printing the time, source file name and line number
     log.SetPrefix("greetings:")
     log.SetFlags(0)
+    names := []string{"Gladys", "Samantha", "Darrin"}
     //requesting a greeting message
-    message, err := greetings.Hello("")
+    messages, err := greetings.Hellos(names)
     // if an error was returned, print it to the console and exit the program
     if err != nil {
         log.Fatal(err)
     }
     
-    fmt.Println(message)
+    fmt.Println(messages)
 }
