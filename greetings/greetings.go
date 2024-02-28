f480e8d9b61f1e374f9de435bcfc997cf22f16bc
KrithikJoshi <aimjkrithik@gmail.com>
2024-02-28 22:28:15 +0530

    handling error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,16 @@
 package greetings
 
-import "fmt"
+import ("fmt"
+		"errors"
 
-func Hello( name string) string{
+)
+
+func Hello( name string) (string, error){
+	//no name given return error message
+	if name == "" {
+		return "" ,errors.New("empty name")
+	}
 	message:=fmt.Sprintf("Hi,%v.Welcome!",name)
-	return message
+	return message, nil
 }
 //func Hello( name string) {string}-> this datatype is of returning value 
--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
 
-    "example.com/greetings"
+	"example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    //setting the properties to predefined logger , including the log entry prefix and a flag to disable printing the time, source file name and line number
+    log.SetPrefix("greetings:")
+    log.SetFlags(0)
+    //requesting a greeting message
+    message, err := greetings.Hello("")
+    // if an error was returned, print it to the console and exit the program
+    if err != nil {
+        log.Fatal(err)
+    }
+    
     fmt.Println(message)
 }
